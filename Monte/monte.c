af1d8b202c1aaa87b596e992637a2861851b4cf4
Fukuda Taiki <s21C1101AP@s.chibakoudai.jp>
2022-12-24 22:33:19 +0900

    fix

--- Monte/monte.c
+++ Monte/monte.c
@@ -5,29 +5,41 @@
 
 int main (){
 	int i;
-	int count = 0; 
-	int long num = 0;
+	int count1 = 0, count2 = 0; 
+	int long num1 = 0, num2 = 0;
 	double x, y;
 	double pi;
 
 	printf("試行回数：");
-	scanf("%ld",&num);
+	scanf("%ld",&num1);
+	printf("繰り返し：");
+	scanf("%ld",&num2);
 
 	srand((unsigned)time(NULL));
+	
+	while(count2 <= num2){
 
-	for (i = 0; i <= num; i++){
-		x = (double)(rand() / (double)RAND_MAX);
-		y = (double)(rand() / (double)RAND_MAX);
-		x = pow(x - 0.5, 2);
-		y = pow(y - 0.5, 2);
+		for (i = 0; i <= num1; i++){
+			x = (double)(rand() / (double)RAND_MAX);
+			y = (double)(rand() / (double)RAND_MAX);
+			x = pow(x - 0.5, 2);
+			y = pow(y - 0.5, 2);
 
-		if(x + y <= pow(0.5, 2)){
-			count++;
+			if(x + y <= pow(0.5, 2)){
+				count1++;
+			}
 		}
-	}
+	
+		pi = (double)count1 / num1 * 4;
+		printf("%f\n", pi);
 
-	pi = (double)count / num * 4;
-	printf("%f\n", pi);
+		x = 0;
+		y = 0;
+		count1 = 0;
+		pi = 0;
+
+		count2++;
+	}
 
 	return 0;
 
